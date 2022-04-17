719ab863705874a157d2775b754893bf9f425bc9
Gabi <gabrielsotir18@yahoo.com>
2022-04-17 14:32:05 +0300

    finish browser Etch-a-sketch

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gridContainer  = document.querySelector(".grid-container");
-
+let clicked;
+document.addEventListener('mousedown', e => clicked = e.buttons);
+document.addEventListener('mouseup', e => clicked = e.buttons);
 
 function draw(gridDimension){
     for(let i = 0 ; i < gridDimension ; i++){
@@ -18,9 +20,26 @@ let dimension;
 const slider = document.getElementById("grid-size-input");
 dimension = document.getElementById("grid-size-input").value;
 draw(dimension);
-
 slider.addEventListener("input", drawGrid);
 
+const gridItems = Array.from(document.querySelectorAll('.grid-element'));
+    gridItems.forEach(item =>{
+        item.addEventListener("mouseover", e =>{
+            if(clicked){
+                changeColor(e.target);
+            }   
+        });
+        item.addEventListener("mousedown", e =>{
+            changeColor(e.target);
+        });
+        item.addEventListener("touchmove", e =>{
+            changeColor(e.target);
+        });
+    });
+
+function changeColor(item){
+    item.style.backgroundColor = "black";
+}
 
 function drawGrid(){
     dimension = document.getElementById("grid-size-input").value;
@@ -28,5 +47,20 @@ function drawGrid(){
         gridContainer.firstElementChild.remove();
     }
     draw(dimension);
-    document.querySelector(".output").textContent = dimension;
+    document.querySelector(".output").textContent = `${dimension} x ${dimension}`;
+    const gridItems = Array.from(document.querySelectorAll('.grid-element'));
+    gridItems.forEach(item =>{
+        item.addEventListener("mouseover", e =>{
+            if(clicked){
+                changeColor(e.target);
+            }    
+        });
+        item.addEventListener("mousedown", e =>{
+            changeColor(e.target);
+        });
+    });
 }
+
+
+
+
